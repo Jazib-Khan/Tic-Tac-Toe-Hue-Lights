c52a7d801f3666ef74f4f57c30399ca99c26c5d2
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2019-12-12 12:33:07 +0000

    Game made
    Created a fully functional tic tac toe board with the ability of playing the game itself.

---
+++ lights.js
@@ -0,0 +1,8 @@
+var bulbIP = "http://192.168.0.50/api/";
+
+var apiKey = "stlaB2I6VZ8080Qepc-1xfmlrHgyTFvB9IGupaQz"; 
+
+var BulbID = ["1", "2" , "3", "4", "5", "6"]
+
+var apiURL = bulbIP + apiKey + "/lights/" + bulb + "/";
+
